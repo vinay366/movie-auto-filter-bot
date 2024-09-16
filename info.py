b34cb1e1a2f428cb14035b76e12f7e49af878fe7
Jeeva1055221 <151960878+Jeeva1055221@users.noreply.github.com>
2024-09-17 00:35:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jeevanantham8157:1055221@heartfilter.rrt16.mongodb.net/?retryWrites=true&w=majority&appName=HeartFilter")
-DATABASE_NAME = environ.get('DATABASE_NAME', HeartFilter")
+DATABASE_NAME = environ.get('DATABASE_NAME', "HeartFilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 
