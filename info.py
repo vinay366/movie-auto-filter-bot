5c53df62cfff1dd71b85648310b1845b6f00f9b6
Jeeva1055221 <151960878+Jeeva1055221@users.noreply.github.com>
2024-09-17 00:52:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jeevanantham8157:1055221@heartfilter.rrt16.mongodb.net/?retryWrites=true&w=majority&appName=HeartFilter")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AutoFilter:1055221@heartfilter.rrt16.mongodb.net/?retryWrites=true&w=majority&appName=HeartFilter")
 DATABASE_NAME = environ.get('DATABASE_NAME', "HeartFilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
