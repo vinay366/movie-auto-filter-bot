04d1f206ffbc97e1f309f2e7835b9f61f9a89d8a
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2024-09-16 13:00:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,13 +20,13 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6006401812:AAEnW3gVaJsJ9MEAdAcwS3mzKIEJmRt
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-#PICS = (environ.get('PICS', 'https://i.imgur.com/4SOqPpL.jpeg')).split() #SAMPLE PIC
-#NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-#MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/1f774a8adb603614fc385.mp4")
-#SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/3207ba97e1879873e54ac.jpg'))
-#SUBSCRIPTIO = (environ.get('SUBSCRIPTION', 'https://graph.org/file/3207ba97e1879873e54ac.jpg')).split()
-#CODE = (environ.get('CODE', 'https://envs.sh/wRt.jpg'))
-#PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/wRt.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.imgur.com/4SOqPpL.jpeg')).split() #SAMPLE PIC
+NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
+MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/1f774a8adb603614fc385.mp4")
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/3207ba97e1879873e54ac.jpg'))
+SUBSCRIPTIO = (environ.get('SUBSCRIPTION', 'https://graph.org/file/3207ba97e1879873e54ac.jpg')).split()
+CODE = (environ.get('CODE', 'https://envs.sh/wRt.jpg'))
+PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/wRt.jpg')).split()
 REFER_PICS = (environ.get("REFER_PICS", "https://graph.org/file/1a2e64aee3d4d10edd930.jpg")).split() 
 
 #stream link shortner
